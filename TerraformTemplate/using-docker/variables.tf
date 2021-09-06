74a26cd408b1ab4de54fd9ca5e6f7df087ddc01a
b0gsbinny <49986512+b0gsbinny@users.noreply.github.com>
2021-09-06 22:07:33 +0300

    Add files via upload

---
+++ TerraformTemplate/using-docker/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "us-east-2a"
+}
+
+variable "ami-id" {
+    default = "ami-04b9e92b5572fa0d1"
+}
+
+variable "keyname" {
+    default = "ELKServer"
+}
+
+variable "user" {
+    default = "ubuntu"
+}
